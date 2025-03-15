8bcf3add51c14c99a71a8ec5d38e54edf6285d19
张海宁 <zhn6818@163.com>
2025-03-15 20:42:21 +0800

    update

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -15,20 +15,22 @@ class CocoDataset(BaseDetDataset):
     """Dataset for COCO."""
 
     METAINFO = {
+        # 'classes':
+        # ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train',
+        #  'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
+        #  'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+        #  'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+        #  'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
+        #  'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+        #  'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork',
+        #  'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
+        #  'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
+        #  'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+        #  'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+        #  'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+        #  'scissors', 'teddy bear', 'hair drier', 'toothbrush'),
         'classes':
-        ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train',
-         'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
-         'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
-         'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
-         'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
-         'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
-         'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork',
-         'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
-         'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
-         'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
-         'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
-         'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
-         'scissors', 'teddy bear', 'hair drier', 'toothbrush'),
+        ('M', 'B', 'P'),
         # palette is a list of color tuples, which is used for visualization.
         'palette':
         [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
